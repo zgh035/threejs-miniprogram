ff0fea3b7b2236885c478330df72d9325174e217
zgh035 <zgh035@qq.com>
2021-08-23 10:40:47 +0800

    Update gltf-loader.js

--- example/loaders/gltf-loader.js
+++ example/loaders/gltf-loader.js
@@ -1,6 +1,6 @@
 export function registerGLTFLoader(THREE) {
 
-  /**
+  /**a
  * @author Rich Tibbett / https://github.com/richtr
  * @author mrdoob / http://mrdoob.com/
  * @author Tony Parisi / http://www.tonyparisi.com/
